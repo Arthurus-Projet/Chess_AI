24ebe4258832faf6bfef7bbed76a9415bf75d094
Arthurus-Projet <arthur.mail.instagram@gmail.com>
2025-12-09 19:53:55 +0100

    feat: create ZobristHashing.h

---
+++ Headers/ZobristHashing.h
@@ -0,0 +1,22 @@
+#ifndef ZOBRIST_H
+#define ZOBRIST_H
+
+#include <random>
+#include <array>
+#include <cstdint>
+
+
+constexpr int NUM_PIECES = 12;
+constexpr int NUM_SQUARES = 64;
+constexpr int NUM_CASTLING_RIGHTS = 16; // 2**4 = 16
+constexpr int NUM_EN_PASSANT = 8;
+
+class ZobristHashing {
+    public:
+        std::array<std::array<uint64_t, NUM_SQUARES>, NUM_PIECES> pieceSquare;
+        std::array<uint64_t, NUM_CASTLING_RIGHTS> castlingRights;
+        std::array<uint64_t, NUM_EN_PASSANT> enPassant;
+        uint64_t sideToMove;
+
+};
+
